b293899520ad0e48ba34db78381bc2220cc3a367
Petr Kobilák <petr.kobilak@tacticaware.com>
2020-11-08 01:07:15 +0100

    set Window status for block raycasting when user using Color Picker

---
+++ Assets/vrtp_demo/Scripts/UI/WindowState.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace vrtp_demo.Scripts.UI
+{
+    [CreateAssetMenu(fileName = "FILENAME", menuName = "MENUNAME", order = 0)]
+    public class WindowState : ScriptableObject
+    {
+        
+    }
+}
