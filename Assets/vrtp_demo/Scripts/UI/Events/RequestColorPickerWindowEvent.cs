715ab8820d334dd8406d3fb853f630500c942cf3
Petr Kobilák <petr.kobilak@tacticaware.com>
2020-11-08 17:49:42 +0100

    add Intro and Tween fade between Intro and Main Window

---
+++ Assets/vrtp_demo/Scripts/UI/Events/RequestColorPickerWindowEvent.cs
@@ -0,0 +1,7 @@
+﻿namespace vrtp_demo.Scripts.UI.Events
+{
+    public class RequestColorPickerWindowEvent
+    {
+        
+    }
+}
---
+++ Assets/vrtp_demo/Scripts/UI/Events/RequestFadeInAndOutEvent.cs
@@ -0,0 +1,9 @@
+﻿using vrtp_demo.Scripts.Common.Events;
+
+namespace vrtp_demo.Scripts.UI.Events
+{
+    public class RequestFadeEvent : AbstractEvent
+    {
+        
+    }
+}
